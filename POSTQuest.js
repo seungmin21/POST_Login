ae93342b5807537f0c53994a270f879361f6426b
ohseungmin <ohmin2886@gmail.com>
2023-10-20 10:57:44 +0900

    queryString 방식 body 분석

--- POSTQuest.js
+++ POSTQuest.js
@@ -1,10 +1,14 @@
 const http = require('http');
+const querystring = require('querystring');
 
 http.createServer((request, response) => {
   if(request.method === 'POST' && request.url === '/sub') {
     let body = '';
     request.on('data', chunk => {
       body += chunk.toString()
+    })
+    request.on('end', () => {
+      querystring.parse(body);
     })
   }
 })
