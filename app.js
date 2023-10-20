492d4cece8de38d2fb305b3f23a1f319f4dc928d
ohseungmin <ohmin2886@gmail.com>
2023-10-20 10:28:00 +0900

    GET 요청 http 서버 생성

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const http = require('http');
+
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('Hello');
+}).listen(3000);
