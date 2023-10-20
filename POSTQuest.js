99ce75117b980b1263283164a8517815191339ab
ohseungmin <ohmin2886@gmail.com>
2023-10-20 10:44:05 +0900

    쿼리스트링 방식으로 body 분석

--- POSTQuest.js
+++ POSTQuest.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const querystring = require('querystring');
 
 const server = http.createServer((request, response) => {
   let body = '';
   request.on('data', chunk => {
     body += chunk.tostring();
+  })
+  request.on('end', () => {
+    querystring.parse(body);
   })
 })
