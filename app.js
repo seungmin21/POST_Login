97eb683f9c04e547e1e0c064b5dc7e967a14a3e7
ohseungmin <ohmin2886@gmail.com>
2023-10-20 16:49:18 +0900

    post 요청 방식으로 편지 창까지 접속 완료

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ http.createServer((request, response) => {
     request.on('end', () => {
       querystring.parse(body);
     })
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(`hello`);
+    fs.readFile('post.html', 'utf8', (err, data) =>{
+      if (err) {
+        console.log("post readFile 작성단락이 틀렸다.")
+      } else {
+        // text/plain(있는 그대로)
+        // text/html로 Content-Type을 지정하면 작성했던 html이 html 내용으로 응답한다.
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end(data);
+      }
+    })
   }
 }).listen(3000)
