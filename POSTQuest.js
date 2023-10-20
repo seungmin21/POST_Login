e1a1a89fcda8a605b09a36d57a6c3766a1a9635d
ohseungmin <ohmin2886@gmail.com>
2023-10-20 11:33:41 +0900

    실행파일이므로 서버 구동을 위해 listen을 작성

--- POSTQuest.js
+++ POSTQuest.js
@@ -5,7 +5,7 @@ http.createServer((request, response) => {
   if(request.method === 'POST' && request.url === '/sub') {
     let body = '';
     request.on('data', chunk => {
-      //  9번째 줄에 무언가 입력한게 아니기 때문에 console.log을 찍어도 아무것도 나오지 않는다.
+      // 9번째 줄에 무언가 입력한게 아니기 때문에 console.log을 찍어도 아무것도 나오지 않는다.
       body += chunk.toString()
     })
     // 마찬가지 본문에 넣을 데이터는 아직 아무것도 넣은게 아니기 때문에 콘솔을 찍어도 아무것도 나오지 않는다.
@@ -15,4 +15,4 @@ http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(`hello`);
   }
-})
+}).listen(4000);
