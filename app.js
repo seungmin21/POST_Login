42cd1a77890a23aaf24338656abccb0e9aca04fc
ohseungmin <ohmin2886@gmail.com>
2023-10-20 16:01:53 +0900

    app.js와 fsRead.js를 병합

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((request, response) => {
   if(request.method === 'GET' && request.url === '/') {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('Hello world');
+    fs.readFile('index.html', 'utf8', (err, data) => {
+      if (err) {
+        console.log("다시 작성");
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(data);
+      }
+    })
   }
 }).listen(3000)
